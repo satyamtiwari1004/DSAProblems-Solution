a9ba5c65675a577b20afb28594e370f234206995
Satyam Tiwari <satyamtiwari1004@gmail.com>
2024-07-29 13:34:41 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.7%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (target == matrix[row][col]) return true;      
+            else if (target > matrix[row][col]) row++;
+            else  col--;
+        }
+        return false;
+    }
+}
