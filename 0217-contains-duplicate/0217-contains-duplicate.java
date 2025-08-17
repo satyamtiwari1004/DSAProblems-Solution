7e47357b023a3bd8bc7150652aceaa0a0f6101a6
Satyam Tiwari <satyamtiwari1004@gmail.com>
2025-08-17 22:14:10 +0530

    Time: 11 ms (91.07%), Space: 60.7 MB (12.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+    // List<String> names = Arrays.asList("Alice", "Bob", "Alice", "Charlie");
+    // Set<String> seen = new HashSet<>();
+    // Set<String> duplicates = names.stream()
+    //                               .filter(e -> !seen.add(e)) // add returns false if element already exists
+    //                               .collect(Collectors.toSet());
+    // duplicates will be ["Alice"]
+
+    // List<Integer> numbers = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
+    // List<Integer> distinctNumbers = numbers.stream()
+    //                                        .distinct()
+    //                                        .collect(Collectors.toList());
+    // distinctNumbers will be [1, 2, 3, 4, 5]
+
         HashSet<Integer> unique = new HashSet<>();
 
         for(int i = 0;i<nums.length;i++){
