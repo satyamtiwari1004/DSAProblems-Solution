f7f870fe7fc90a5a3772b58132c9763f5b6fd2dc
Satyam Tiwari <satyamtiwari1004@gmail.com>
2025-08-17 23:10:35 +0530

    Time: 3 ms (91.95%), Space: 42.7 MB (99.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,60 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() !=t.length()){
-            return false;
-        }
 
-        String s1=s.chars().sorted().mapToObj(x->String.valueOf((char) x)).collect(Collectors.joining());
-        String t1=t.chars().sorted().mapToObj(x->String.valueOf((char) x)).collect(Collectors.joining());
+// 	• Same index → fuse into one loop.
+// 	• Different indices → separate into two loops.
+// 	• Same array + same index → best case for fusion.
+//    Different arrays + different indices → best case for separation.
+        // if(s.length() !=t.length()){
+        //     return false;
+        // }
 
-        if(s1.equals(t1)){
+        // String s1=s.chars().sorted().mapToObj(x->String.valueOf((char) x)).collect(Collectors.joining());
+        // String t1=t.chars().sorted().mapToObj(x->String.valueOf((char) x)).collect(Collectors.joining());
+
+        // if(s1.equals(t1)){
+        //     return true;
+        // } //beats 9%
+
+        // HashMap<Character,Integer> map = new HashMap<>();
+        // if(s.length()==t.length()){
+        //     for(int i=0;i<s.length();i++){
+        //         if(map.containsKey(s.charAt(i))){
+        //             map.put(s.charAt(i),map.get(s.charAt(i))+1);
+        //         }else{
+        //             map.put(s.charAt(i),1);
+        //         }
+        //         if(map.containsKey(t.charAt(i))){
+        //             map.put(t.charAt(i),map.get(t.charAt(i))-1);
+        //         }else{
+        //             map.put(t.charAt(i),-1);
+        //         }
+        //     }
+        //     return !map.values().stream().anyMatch(x -> x!=0);
+        // } //beats 6%
+
+        // if(s.length()==t.length()){
+        //     int[] charSet = new int[26];
+        //     for(int i=0;i<s.length();i++){
+        //         charSet[s.charAt(i)-'a']++;
+        //         charSet[t.charAt(i)-'a']--;
+        //     }
+        //     for(int val : charSet) { if(val!=0) return false;}
+        //     return true;
+        // }
+
+        //  return false; //beats 42.35%
+        if(s.length()!=t.length()){return false;}
+            int[] charSet = new int[26];
+            for(int i=0;i<s.length();i++){
+                charSet[s.charAt(i)-'a']++;
+            }
+            for(int i=0;i<t.length();i++){
+                charSet[t.charAt(i)-'a']--;
+            }
+
+            for(int val : charSet) { if(val!=0) return false;}
             return true;
-        }
-
-        return false;
-
+        
     }
 }
